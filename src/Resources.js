e782bd2005258cf518d422371845f19411862c49
Vishrut Aggarwal <vishrut.agg@gmail.com>
2022-04-17 07:24:57 +0530

    created structure

---
+++ src/Resources.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Resources = () => {
+  return (
+    <div id="resources">
+        <div id="resources-card">
+            <div id="resource-card-header">
+                <h3>Resources</h3>
+            </div>
+            <div id="resource-options">
+
+            </div>
+        </div>
+        <div id="resource-page-image">
+            <img src="#" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Resources
